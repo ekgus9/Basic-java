e7d954b7c4ef638ad9a880d5096bc1ce96c63312
ekgus9 <dhaabb55@naver.com>
2021-09-05 13:13:43 +0900

    class, instance, inheritance!

---
+++ Instance.java
@@ -0,0 +1,23 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class Instance {
+
+	public static void main(String[] args) throws FileNotFoundException {
+
+		//class
+		System.out.println(Math.PI); 
+		
+		// instance
+		PrintWriter p = new PrintWriter("result.txt");
+		p.write("HELLO");
+		p.close();
+		
+		
+		//inheritance »ó¼Ó
+		//Object -> Writer -> PrintWriter
+		
+		
+	}
+
+}
