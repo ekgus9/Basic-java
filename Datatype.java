a435b679530d2d8093e2b8a3a8e100a9fe711e75
ekgus9 <dhaabb55@naver.com>
2021-09-05 13:07:13 +0900

    data types~

---
+++ Datatype.java
@@ -0,0 +1,31 @@
+public class Datatype{
+	public static void main (String[] args) {
+		
+		
+		System.out.println(); // /sout/ + ctrl +space
+		
+		
+		// 1. number
+		
+		System.out.println(6/2); // operator 연산자
+		
+		System.out.println(Math.floor(Math.PI)); // up
+		System.out.println(Math.ceil(Math.PI)); // down
+		
+		
+		// 2. string
+		
+		System.out.println('h'); // character : 한 글자를 표현하는 데이터 타입
+		
+		System.out.println("6"+"6"); // string combination
+		
+		System.out.println("1111".length()); // 4
+		// System.out.println(1111.length()); // error
+		
+		System.out.println("hello \nworld"); // new line
+		System.out.println("hello \"world\""); // escape
+		
+		System.out.println("Hello!! My name is ...".replace("...", "ekgus9"));
+		
+	} 
+}
